7ddaf857547fecfc58b30a122c0e8953272d5cd3
Trevor Ogg <trevorogg@Trevors-MacBook-Air.local>
2018-03-11 19:55:20 -0600

    added question info and additional styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,13 +5,13 @@ $(document).ready(function () {
     var HTML;
 
     // Arrays for questions, answers, and correct answers
-    var questions = ["How many google searches are there each day?", 
-    "At it's peak, AOL signed up a new user every:", 
-    "Every second, twitter users type out: ", 
-    "The most popular web browser is: ",
-    "The country with the fastest internet speeds is: ",
-    "In what year did the Facebook overtake Myspace as the most popular social network?",
-    "How many songs are streamed on Spotify every day?"];
+    var questions = ["How many Google searches are there each day?",
+        "At it's peak, AOL signed up a new user every:",
+        "Every second, twitter users type out: ",
+        "The most popular web browser is: ",
+        "The country with the fastest internet speeds is: ",
+        "In what year did the Facebook overtake Myspace as the most popular social network?",
+        "How many songs are streamed on Spotify every day?"];
 
     var answers = [['1.8 billion', '3.5 billion', '500 million', '6 billion'],
     ['0.5 seconds', '6 seconds', '30 seconds', 'Two minutes'],
@@ -19,10 +19,19 @@ $(document).ready(function () {
     ['Google Chrome', 'Firefox', 'Internet Explorer', 'Safari'],
     ['France', 'United States', 'Brazil', 'South Korea'],
     ['2003', '2008', '2011', '2015'],
-    ['25 million', '250 million', '1 billion', '10 billion']
-
-    ];
+    ['25 million', '250 million', '1 billion', '10 billion']];
+    
     var correctAnswers = ['B. 3.5 billion', 'B. 6 seconds', 'D. 6000 tweets', 'A. Google Chrome', 'D. South Korea', 'B. 2008', 'C. 1 billion'];
+
+    var answerInfo = ['There are 3.5 billion Google searches every day, with an average of 40,000 every second!',
+    'During the peak of AOL, a new user signed up every six seconds. The company distributed over 1 billion trial CDs across the world.',
+    'There have been an estimated 146 billion tweets created since Twitter was founded on March 21st, 2006.',
+    'Google chrome overtook Internet Explorer as the most popular web browser in Early 2012. Currently an estimated 57% of internet users choose Google chrome.',
+    'South Koreans enjoy the fastest internet speeds in the world, with an average connection speed of 28.6 Mb/s',
+    'The number of Facebook users surpassed Myspace users in 2008, there are now over 2.2 active Facebook accounts worldwide',
+    'Spotify users stream 1 billion songs every day, Spotify delivers over half of the worlds streaming music at any given time.'];
+
+    var images = ['google.jpg'];
 
     // Variable to track which question 
     var numQuestion = 0;
@@ -79,14 +88,14 @@ $(document).ready(function () {
     }
 
     function correctAns() {
-        HTML = '<p>Correct!</p>';
+        HTML = '<p>Correct!</p>' + '<p>' + answerInfo[numQuestion] + '</p>';
         $('.mainArea').html(HTML);
         setTimeout(wait, 4000);
         numCorrect++;
     }
 
     function incorrectAns() {
-        HTML = '<p>Incorrect!</p>';
+        HTML = '<p>Incorrect!</p>' + '<p>The correct answer was: ' + correctAnswers[numQuestion];
         $('.mainArea').html(HTML);
         setTimeout(wait, 4000);
         numIncorrect++;
@@ -100,8 +109,8 @@ $(document).ready(function () {
     }
 
     function endGame() {
-       HTML = '<p>Number correct: ' + numCorrect + '</p>' + '<p>Number incorrect: ' + numIncorrect + '</p>' + '<p>Number skipped: ' + numSkipped + '</p>' + "<p class='text-center'> <a class='btn btn-primary btn-lg newGame'> Try again? </a></p>";
-       $('.mainArea').html(HTML);
+        HTML = '<p>Number correct: ' + numCorrect + '</p>' + '<p>Number incorrect: ' + numIncorrect + '</p>' + '<p>Number skipped: ' + numSkipped + '</p>' + "<p class='text-center'> <a class='btn btn-primary btn-lg newGame'> Try again? </a></p>";
+        $('.mainArea').html(HTML);
     }
 
     function newGame() {
@@ -139,7 +148,7 @@ $(document).ready(function () {
 
     });
 
-    $('body').on('click', '.newGame', function (event){
+    $('body').on('click', '.newGame', function (event) {
         newGame();
     });
 
